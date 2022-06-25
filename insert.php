41e46e438d68ab650c16263a794815d222b201d7
s26244 <s26244@pjwstk.edu.pl>
2022-06-25 23:39:47 +0200

    Dopracowana możliwość dodawania nowych filmów

--- insert.php
+++ insert.php
@@ -1,1 +1,34 @@
 <?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "projektdb";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+
+
+
+
+
+$sql= "INSERT INTO `movies`( `title`, `description`, `image`, `category`, `review`, `release_date`, `director`, `major_actors`, `link`) VALUES ('".$_POST['title']."','".$_POST['description']."','".$_POST['image']."','".$_POST['category']."','".$_POST['review']."','".$_POST['release_date']."','".$_POST['director']."','".$_POST['major actors']."','".$_POST['link']."')";
+
+
+echo "<script>window.location.href='index.php';</script>";
+
+if (mysqli_query($conn,$sql)) {
+    echo "<h1 style='color:green'>Successfully added new movie</h1>";
+    echo "<script>window.location.href='index.php';</script>";
+}
+else{
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+?>
